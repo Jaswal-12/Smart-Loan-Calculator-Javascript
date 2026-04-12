884955e62a9991d870f09b4ecf34a848a75bba16
Karan Jaswal <karanosr2004@gmail.com>
2026-04-12 18:16:23 +0530

    Initial commit - Number Adder Project

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
- const btn1 = document.querySelector(".btn1");
-const btn2 = document.querySelector(".btn2");
-const title = document.querySelector(".title");
-const resetBtn = document.querySelector(".reset");
-const body = document.querySelector("body");
+const add = document.querySelector('#add');
+const i1 = document.querySelector("#i1");
+const i2 = document.getElementById("i2");
+const result = document.getElementById("result");
 
-const colors = ["red", "blue", "green", "yellow", "purple", "orange"];
-
-btn1.addEventListener('click', () => {
-    const random = colors[Math.floor(Math.random() * colors.length)];
-    
-    // use background instead of backgroundColor
-    body.style.background = random;
-});
-
-btn2.addEventListener('click', () => {
-    title.style.fontSize = "90px";
-});
-
-resetBtn.addEventListener('click', () => {
-    title.style.fontSize = "";
-    
-    // reset to original gradient
-    body.style.background = "linear-gradient(135deg, #667eea, #764ba2)";
+add.addEventListener('click', function () {
+  const sum = Number(i1.value) + Number(i2.value);
+  result.innerHTML = sum;
+  i1.value="";
+  i2.value="";
 });
